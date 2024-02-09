77428d1049b1cb87db50cedab6ed21073da3cdea
Ihor Buhaichuk <ihorbugaichuk@gmail.com>
2024-02-09 23:48:46 +0200

    Clsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,15 +1,22 @@
 import "./Alert.css";
+import clsx from "clsx";
   
   export const Alert = ({ variant, outlined, elevated, children }) => {
-    const classNames = ["alert", variant];
-  
-    if (outlined) {
-          classNames.push("is-outlined");
-      }
-  
-    if (elevated) {
-          classNames.push("is-elevated");
-      }
-  
-    return <p className={classNames.join(" ")}>{children}</p>;
+    return (
+      <p
+        className={clsx("alert", variant, {
+          "is-outlined": outlined,
+          "is-elevated": elevated,
+        })}
+      >
+        {children}
+      </p>
+    );
   };
+  
+  
+
+
+//   Для обчислення фінального значення атрибуту className можна було використати блок if...else, 
+//   інструкцію switch, тернарний оператор або будь-який інший синтаксис JavaScript, який дасть нам 
+//   той же результат. Головне, щоб рядок з класами був складений правильно і не мав зайвих чи невалідних значень.
