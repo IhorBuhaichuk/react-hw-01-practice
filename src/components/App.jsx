cb551a7c0f8b384298076ef598a2ff6f368acbfa
Ihor Buhaichuk <ihorbugaichuk@gmail.com>
2024-02-09 21:05:22 +0200

    Created first props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,22 @@
-// src/App.jsx
-import {Product} from "./Product";
+// src/components/App.jsx
+
+import { Product } from "./Product";
 
 export default function App() {
   return (
     <div>
-      <h1>Products</h1>
+      <h1>Best selling</h1>
 
-      <Product />
-      <Product />
-      <Product />
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/6/14/0/MRS-DASH_Fiesta-Lime-Tacos_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371593390902.jpeg"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://d9hyo6bif16lx.cloudfront.net/live/img/production/detail/menu/lunch-dinner_999-combohs_all-american-burger-fries.jpg"
+        price={14.29}
+      />
     </div>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,12 @@
-// src/Product.jsx
+// src/components/Product.jsx
 
-export const Product = () => {
-    return (
-      <div>
-        <h2>Tacos</h2>
-              <p>Price: 999 credits</p>
-      </div>
-    );
-  };
-  
+export const Product = ({ name, imgUrl, price }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+			<img src={imgUrl} alt={name} width="480" />
+			<p>Price: {price} credits</p>
+    </div>
+  );
+};
+
